7ffcab7e4829bb024cb687094e706681d5a85763
alinnecardoso <dev14cardoso@gmail.com>
2024-02-15 16:32:55 -0300

    Step3

--- Desenvolvimento_Web/multi-step-form-main/script/CheckStep3.js
+++ Desenvolvimento_Web/multi-step-form-main/script/CheckStep3.js
@@ -1,3 +1,4 @@
+// Função para verificar se um checkbox está marcado
 function isChecked(OnOff) {
     var inputElement = document.getElementById(OnOff);
     if (inputElement && inputElement.type === "checkbox") {
@@ -7,50 +8,48 @@ function isChecked(OnOff) {
     }
 }
 
-
+// Função para somar os valores dos checkboxes marcados
 function somaCheck() {
+    // Valor do PlanStep2
+    var valor = getValorSelecionado();
+
+    var checkbox1 = document.querySelector("#checkbox1");
+    var checkbox2 = document.querySelector("#checkbox2");
+    var checkbox3 = document.querySelector("#checkbox3");
 
     var valor1 = document.querySelector("#valor1");
     var valor2 = document.querySelector("#valor2");
     var valor3 = document.querySelector("#valor3");
 
-    var checkbox1 = document.querySelector("#checkbox1");
-    var checkbox2 = document.querySelector("#checkbox2");
-    var checkbox3 = document.querySelector("#checkbox3");
-    
     var soma = 0;
-    var qtdCheck = document.querySelectorAll("[name='opcaoStep3]'");
-    alert(qtdCheck.values)
-    
+    var qtdCheck = document.querySelectorAll("[name='opcaoStep3']");
+
     if (isChecked('OnOff') === true) {
+        checkbox1.textContent = "$10/yr";
+        valor1.value = "10";
 
-        checkbox1.textContent = "$10/yr"
-        valor1.value = "10"
+        checkbox2.textContent = "$20/yr";
+        valor2.value = "20";
 
-        checkbox2.textContent = "$20/yr"
-        valor2.value = "20"
-
-        checkbox3.textContent = "$20/yr"
-        valor3.value = "20"
+        checkbox3.textContent = "$20/yr";
+        valor3.value = "20";
 
         for (var i = 0; i < qtdCheck.length; i++) {
             if (qtdCheck[i].checked) {
                 soma += Number(qtdCheck[i].value);
             }
         }
-        alert(soma);
-
+        
     }
 
     if (isChecked('OnOff') === false) {
-        
-        checkbox1.textContent = "$1/mo"
-        valor1.value = "1"
+        checkbox1.textContent = "$1/mo";
+        valor1.value = "1";
 
-        checkbox2.textContent = "$2/mo"
-        valor2.value = "2"
+        checkbox2.textContent = "$2/mo";
+        valor2.value = "2";
 
-        checkbox3.textContent = "$2/mo"
+        checkbox3.textContent = "$2/mo";
         valor3.value = "2";
 
         for (var i = 0; i < qtdCheck.length; i++) {
@@ -58,9 +57,13 @@ function somaCheck() {
                 soma += Number(qtdCheck[i].value);
             }
         }
-        alert(soma);
-
         
     }
+    
+    alert(soma+ Number(valor));
 }
+
+
+
+
 
